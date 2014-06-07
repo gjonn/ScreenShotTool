16435283f4ed1b84ce87b668df47fd1a7b28487e
gjonn <gjonn@hotmail.com>
2014-06-06 23:36:46 -0400

    Added files

---
+++ ScreenShotTool/Dialog.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Live;
+
+namespace ScreenShotTool
+{
+    public partial class Dialog : Form
+    {
+        public Dialog()
+        {
+            InitializeComponent();
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage; // makes the picture the size of the picturebox
+            pictureBox1.Image = Form1.bit; // sets image to picturebox
+            this.Text = "Screenshot"; // changes form name
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // saves the image to a file
+            SaveFileDialog sv = new SaveFileDialog();
+            sv.Filter = "Images|*.png";
+            ImageFormat format = ImageFormat.Png;
+            if (sv.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Form1.bit.Save(sv.FileName, format);
+
+            }
+        }
+
+    }
+}
---
+++ ScreenShotTool/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ScreenShotTool
+{
+    public partial class Form1 : Form
+    {
+        public static Bitmap bit = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height); 
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Screenshot Tool";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            this.Hide(); //hides form so it's not in the screenshot
+            timer1.Interval = 200; //200ms delay
+            timer1.Enabled = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false; //stops timer
+            Graphics SS = Graphics.FromImage(bit);
+            SS.CopyFromScreen(0, 0, 0, 0, bit.Size);
+
+            // opens screenshot form
+            Dialog Dg = new Dialog();
+            Dg.ShowDialog();
+        }
+    }
+}
